274d6388777627d13cdac64304a6578f2ff335f3
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2018-02-06 22:00:50 +0900

    add comment

--- src/Hakyll/Web/Liquid.hs
+++ src/Hakyll/Web/Liquid.hs
@@ -18,18 +18,22 @@ import Hakyll
 import Hakyll.Core.Compiler
 import qualified Text.Liquid as Liquid
 
+-- | Parse underlying item and compile it with its metadata as context.
 parseAndInterpretDefault :: Compiler (Item String)
 parseAndInterpretDefault = do
   metadata <- getMetadata =<< getUnderlying
   item <- getResourceBody
   parse item >>= interpret metadata
 
+-- | Parse underlying item and compile it with given metadata as context.
 parseAndInterpret :: Metadata -> Compiler (Item String)
 parseAndInterpret metadata = getResourceBody >>= parse >>= interpret metadata
 
+-- | Parse underlying item and compile it with given context.
 parseAndInterpret' :: Aeson.Value -> Compiler (Item String)
 parseAndInterpret' context = getResourceBody >>= parse >>= interpret' context
 
+-- | Parse given item.
 parse :: Item String -> Compiler (Item [Liquid.Expr])
 parse (Item identifier body) =
   case Liquid.parseTemplate (Text.pack body) of
@@ -41,9 +45,11 @@ parse (Item identifier body) =
         Attoparsec.Fail _ ctx msg -> throwError $ msg:ctx
     Attoparsec.Fail _ ctx msg -> throwError $ msg:ctx
 
+-- | Compile Liquid expressions with given metadata as context.
 interpret :: Metadata -> Item [Liquid.Expr] -> Compiler (Item String)
 interpret metadata = interpret' (Aeson.Object metadata)
 
+-- | Compile Liquid expressions with given context.
 interpret' :: Aeson.Value -> Item [Liquid.Expr] -> Compiler (Item String)
 interpret' context (Item identifier expressions) =
   case Liquid.interpret context expressions of
